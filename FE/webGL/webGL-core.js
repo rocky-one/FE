11749f6b0c2ac2b6c5bd235abfff0cc38e895840
rocky-one <zuihongyanxiangsi@163.com>
2022-04-04 07:25:53 +0800

    webgl纹理

--- FE/webGL/webGL-core.js
+++ FE/webGL/webGL-core.js
@@ -25,3 +25,32 @@ function createProgram(webgl, vertextShaderObject, fragmentShaderObject) {
 
     return programObject
 }
+
+function onLoadImage(src, cb) {
+    const image = new Image()
+    image.crossOrigin = 'anonymous'
+    image.src = src;
+    image.onload = function() {
+        cb && cb(image)
+    }
+}
+// 创建纹理
+function createTexture(webgl, src) {
+    const texture = webgl.createTexture()
+    texture.image = new Image()
+    texture.image.src = src;
+    texture.image.onload = function() {
+        bindTexture(webgl, texture)
+    }
+    return texture
+}
+
+// 绑定处理纹理
+function bindTexture(webgl, texture) {
+    webgl.bindTexture(webgl.TEXTURE_2D, texture)
+    webgl.texImage2D(webgl.TEXTURE_2D, 0, webgl.RGBA, webgl.RGBA, webgl.UNSIGNED_BYTE, texture.image)
+    webgl.texParameteri(webgl.TEXTURE_2D, webgl.TEXTURE_MAG_FILTER, webgl.NEAREST)
+    webgl.texParameteri(webgl.TEXTURE_2D, webgl.TEXTURE_MIN_FILTER, webgl.NEAREST)
+    webgl.texParameteri(webgl.TEXTURE_2D, webgl.TEXTURE_WRAP_S, webgl.CLAMP_TO_EDGE)
+    webgl.texParameteri(webgl.TEXTURE_2D, webgl.TEXTURE_WRAP_T, webgl.CLAMP_TO_EDGE)
+}
